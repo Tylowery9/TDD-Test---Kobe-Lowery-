31d08aae400789d1e27b00fe299bf10096e66c9b
Tylowery9 <112112477+Tylowery9@users.noreply.github.com>
2022-10-26 19:12:49 -0400

    This is commit 1

---
+++ animal.py
@@ -0,0 +1,12 @@
+class Animal():
+    def init(self, type, size, age , name) : 
+        self.type = ""
+        self.size = 0
+        self.age = 0
+        self.name = ""
+
+    def speak(self):
+        pass
+    
+    def describe(self):
+        pass
