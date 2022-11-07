0d9590f20bc138d64a442786cf77bae7c8505cb0
Tylowery9 <112112477+Tylowery9@users.noreply.github.com>
2022-11-07 15:39:00 -0500

    This is a test commit

--- animal.py
+++ animal.py
@@ -1,5 +1,5 @@
 class Animal():
-    def init(self, ctype):
+    def __init__(self, ctype):
         self.type = ctype
         self.size = ""
         self.age = 0
@@ -8,7 +8,23 @@ class Animal():
         if self.type == "cat":
             self.name = "Seymour"
 
+        if self.type == "dog":
+            self.name = "Spot"
+
+        
+#WORK ON THIS
     def speak(self):
+        #if is cat
+        if self.type == cat:
+            print("MEOW")
+            if self.size == "small":
+                print("meow")
+            #if is small
+            if self.size == "small":
+                print("meow") 
+                #return meow
+            return self.speak(self)
+        #elif is ....
         pass
     
     def describe(self):
